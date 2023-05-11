0eafbbea782e62d3de6c25b39e05843956ee6e0a
Cam206 verhey <camryn.verhey@gmail.com>
2023-05-11 18:42:05 +0000

    questions are done

--- src/routes/questions.js
+++ src/routes/questions.js
@@ -7,34 +7,96 @@
  * }[] }
  */
 export const questions = [
-    {
-        question: "This is the first question.",
-        answers: [
-            "True",
-            "False"
-        ],
-        correct: 0,
-        time: 20
-    },
-    {
-        question: "Who made this question?",
-        answers: [
-            "Ben Thomas",
-            "Mr. Bonner",
-            "Sam Mote",
-            "Camryn Verhey"
-        ],
-        correct: 1,
-        time: 20
-    },
-    {
-        question: "How much time is left in this class?",
-        answers: [
-            "So much",
-            "A respectable amount",
-            "Not enough"
-        ],
-        correct: 2,
-        time: 20
-    }
-]
+        {
+            question: " Apple was founded before Microsoft.",
+            answers: [
+                "True",
+                "False"
+            ],
+            correct: 1,
+            time: 20
+        },
+        {
+            question: "what is the number system comprised solely of 0 and 1s called?",
+            answers: [
+                "Base 0",
+                "Base 2",
+                "Base 3"
+            ],
+            correct: 1,
+            time: 20
+        },
+        {
+            question: "what does I/O stand for?",
+            answers: [
+                "internal/ online",
+                "input/ output"
+            ],
+            correct: 1,
+            time: 20
+        },
+        {
+            question: "CLI stands for Command Line Interface.",
+            answers: [
+                "True",
+                "False"
+            ],
+            correct: 0,
+            time: 20
+        },
+        {
+            question: " who is responsible for creating the original version of MacOS",
+            answers: [
+                "Steve Jobs",
+                "President Joe Biden",
+                "Bill Gates"
+            ],
+            correct: 2,
+            time: 20
+        },
+        {
+            question: "What was the original meaning of the term Computer?",
+            answers: [
+                "An early mechanical calculator",
+                "A person who did computations professionally"
+            ],
+            correct: 1,
+            time: 20
+        },
+        {
+            question: "Seve Wozniak is the co-founder of Apple Computers alongside Steve Jobs.",
+            answers: [
+                "True",
+                "False"
+            ],
+            correct: 0,
+            time: 20
+        },
+        {
+            question: "which one is an opening tag?.",
+            answers: [
+                "<p>",
+                "</p>"
+            ],
+            correct: 0,
+            time: 20
+        },
+        {
+            question: "the color red makes people think of danger or it grabs their attention.",
+            answers: [
+                "True",
+                "False"
+            ],
+            correct: 0,
+            time: 20
+        },
+        {
+            question: "The tech ladder is a scale of products that change price very little and makes you want to buy the next one up.",
+            answers: [
+                "True",
+                "False"
+            ],
+            correct: 0,
+            time: 20
+        },
+    ]
